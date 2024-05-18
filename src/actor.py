2ec3d8b1de4a9609c23e9b1736905e43a048cb33
Chris Ohk <utilforever@gmail.com>
2024-05-18 15:34:20 +0900

    feat(actor): Add class 'Actor'

---
+++ src/actor.py
@@ -0,0 +1,187 @@
+import pyxel
+
+import rect
+import sprite
+import app
+
+DEFAULT_GRAVITY = 0.5
+DEFAULT_MAX_FALL_SPEED = 2.5
+
+
+def empty_callback():
+    pass
+
+
+class Actor:
+    def __init__(self, world, props, x, y):
+        self.type = props["type"]
+        self.world = world
+        self.sprite = sprite.Sprite(props, x, y)
+        self.hitbox = rect.Rect(*(props.get("hitbox", (8, 8, 8, 8))))
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.gravity = DEFAULT_GRAVITY
+        self.max_fall_speed = DEFAULT_MAX_FALL_SPEED
+        self.is_falling = False
+        self.uid = 0
+        self.children = []
+
+        self.hit_wall_callback = empty_callback
+        self.hit_roof_callback = empty_callback
+        self.hit_floor_callback = empty_callback
+
+        self.apply_gravity_func = None
+        self.set_flip_func = None
+
+        self.got_hit = None
+        self.can_get_hit = None
+
+        self.on_dead_removal_func = None
+
+    def get_hitbox(self):
+        x = self.sprite.position[0] + self.hitbox.left
+        y = self.sprite.position[1] + self.hitbox.top
+
+        return rect.Rect(x, y, self.hitbox.w, self.hitbox.h)
+
+    def check_hit(self, params):
+        if (
+            self.got_hit is None
+            or self.can_get_hit is None
+            or self.can_get_hit() is False
+        ):
+            return
+
+        hitbox = params["hitbox"]
+        selfbox = self.get_hitbox()
+
+        if selfbox.is_overlapping_other(hitbox):
+            self.got_hit(params)
+            return True
+
+        return False
+
+    def get_state(self):
+        return self.sprite.state
+
+    def set_state(self, state):
+        self.sprite.set_state(state)
+
+    def move(self):
+        last_y = self.sprite.position[1]
+
+        self.apply_gravity()
+
+        pos = self.sprite.position
+        pos[0], pos[1] = self.push_back(
+            pos[0], pos[1], self.velocity_x, self.velocity_y
+        )
+
+        self.is_falling = self.sprite.position[1] > last_y
+
+    def apply_gravity(self):
+        if self.apply_gravity_func:
+            self.apply_gravity_func()
+        else:
+            self.velocity_y = min(self.velocity_y + self.gravity, self.max_fall_speed)
+
+    def detect_collision(self, x, y, dy=0):
+        x1 = int((x + self.hitbox.left) // 8)
+        y1 = int((y + self.hitbox.top) // 8)
+        x2 = int((x + self.hitbox.right - 1) // 8)
+        y2 = int((y + self.hitbox.bottom - 1) // 8)
+
+        for yi in range(y1, y2 + 1):
+            for xi in range(x1, x2 + 1):
+                if self.world.map.is_solid(xi, yi):
+                    return True
+
+        if dy > 0 and pyxel.floor((y + self.hitbox.bottom) % 8) == 1:
+            for xi in range(x1, x2 + 1):
+                if self.world.map.is_one_way(xi, y2):
+                    return True
+
+        return False
+
+    def push_back_x(self, x, y, dx, abs_dx):
+        total_moved_abs = 0
+        sign = 1 if dx > 0 else -1
+
+        while total_moved_abs < abs_dx:
+            d = min(1, abs_dx - total_moved_abs) * sign
+
+            if self.detect_collision(x + d, y):
+                self.hit_wall_callback()
+                break
+
+            x += d
+            total_moved_abs += abs(d)
+
+        return x
+
+    def push_back_y(self, x, y, dy, abs_dy):
+        total_moved_abs = 0
+        sign = 1 if dy > 0 else -1
+
+        while total_moved_abs < abs_dy:
+            d = min(1, abs_dy - total_moved_abs) * sign
+
+            if self.detect_collision(x, y + d, dy):
+                if dy > 0:
+                    self.hit_floor_callback()
+                else:
+                    self.hit_roof_callback()
+                break
+
+            y += d
+            total_moved_abs += abs(d)
+
+        return y
+
+    def push_back(self, x, y, dx, dy):
+        abs_dx = abs(dx)
+        abs_dy = abs(dy)
+
+        if abs_dx > abs_dy:
+            x = self.push_back_x(x, y, dx, abs_dx)
+            y = self.push_back_y(x, y, dy, abs_dy)
+        else:
+            y = self.push_back_y(x, y, dy, abs_dy)
+            x = self.push_back_x(x, y, dx, abs_dx)
+
+        return x, y
+
+    def set_flip(self):
+        if self.set_flip_func:
+            self.set_flip_func()
+        else:
+            sign = pyxel.sgn(self.velocity_x)
+
+            if sign < 0:
+                self.sprite.flip_h = True
+            elif sign > 0:
+                self.sprite.flip_h = False
+
+    def update(self):
+        self.move()
+        self.sprite.update()
+        self.set_flip()
+
+        for child in self.children:
+            child.update()
+
+    def draw(self):
+        cam = self.world.map.cam
+        self.sprite.draw(cam)
+
+        for child in self.children:
+            child.draw()
+
+        if app.g_debug:
+            pyxel.rectb(
+                pyxel.floor(self.sprite.position[0] + self.hitbox.left - cam.rect.left),
+                pyxel.floor(self.sprite.position[1] + self.hitbox.top - cam.rect.top),
+                self.hitbox.w,
+                self.hitbox.h,
+                8,
+            )
