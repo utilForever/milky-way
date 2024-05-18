b7869c961769e4402635cec1e9d8b2f2ae10cb0c
Chris Ohk <utilforever@gmail.com>
2024-05-18 15:43:44 +0900

    feat(actor): Add class 'Climb' for state

---
+++ src/player/climb_state.py
@@ -0,0 +1,80 @@
+import pyxel
+
+import state
+import actor
+
+CLIMB_SPEED = 0.6
+
+
+class Climb(state.State):
+    def __init__(self, player, state_machine, world) -> None:
+        super().__init__()
+
+        self.name = "climb"
+        self.player = player
+        self.state_machine = state_machine
+        self.world = world
+
+    def enter(self, enter_params=None):
+        self.player.sprite.set_state("climb")
+        self.player.gravity = 0
+        self.player.max_fall_speed = 0
+        self.player.velocity_x = 0
+        self.player.velocity_y = 0
+        self.is_falling = False
+        self.player.velocity_y_tween = None
+
+        self.player.hit_floor_callback = self.climb_hit_floor
+        self.player.apply_gravity_func = actor.empty_callback
+        self.player.set_flip_func = actor.empty_callback
+
+    def climb_hit_floor(self):
+        self.state_machine.change("idle")
+
+    def exit(self):
+        self.player.gravity = actor.DEFAULT_GRAVITY
+        self.player.max_fall_speed = actor.DEFAULT_MAX_FALL_SPEED
+        self.player.hit_floor_callback = actor.empty_callback
+        self.player.apply_gravity_func = None
+        self.player.set_flip_func = None
+
+    def handle_input(self, inputs):
+        if inputs.pressing("button_jump"):
+            self.state_machine.change("fall")
+            return
+
+        move_x = 0
+
+        if inputs.pressing("left"):
+            move_x += -1
+        if inputs.pressing("right"):
+            move_x += 1
+
+        move_y = 0
+
+        if inputs.pressing("up"):
+            move_y += -1
+        if inputs.pressing("down"):
+            move_y += 1
+
+        climb_speed = CLIMB_SPEED
+
+        if move_x != 0 and move_y != 0:
+            climb_speed *= 0.707
+
+        if move_x != 0 or move_y != 0:
+            if pyxel.frame_count % 10 == 0:
+                self.player.sprite.flip_h = not self.player.sprite.flip_h
+
+        self.player.velocity_x = move_x * climb_speed
+        self.player.velocity_y = move_y * climb_speed
+
+    def update(self):
+        self.is_falling = False
+        tile = self.player.get_touching_climbable()
+
+        if tile is None:
+            self.state_machine.change("idle")
+
+    def draw(self):
+        pass
