b363e84e5b92f03b4d116b83972977c014bcf501
Chris Ohk <utilforever@gmail.com>
2024-05-18 15:42:40 +0900

    feat(actor): Add class 'Run' for state

---
+++ src/player/run_state.py
@@ -0,0 +1,56 @@
+import state
+
+
+class Run(state.State):
+    def __init__(self, player, state_machine, world) -> None:
+        super().__init__()
+
+        self.name = "run"
+        self.player = player
+        self.state_machine = state_machine
+        self.world = world
+
+    def enter(self, enter_params=None):
+        self.player.sprite.set_state("run")
+
+    def exit(self):
+        pass
+
+    def handle_input(self, inputs):
+        if inputs.tapped("button_talk"):
+            self.player.velocity_x = 0
+            self.state_machine.change("talk")
+            return
+
+        if inputs.pressing("button_jump"):
+            self.state_machine.change("jump")
+            return
+
+        if inputs.pressing("up"):
+            tile = self.player.get_touching_climbable()
+
+            if tile is not None:
+                self.state_machine.change("climb")
+
+            return
+
+        move_x = 0
+
+        if inputs.pressing("left"):
+            move_x += -1
+
+        if inputs.pressing("right"):
+            move_x += 1
+
+        self.player.velocity_x = move_x * self.player.run_speed
+
+        if move_x == 0:
+            self.state_machine.change("idle")
+            return
+
+    def update(self):
+        if self.player.is_falling:
+            self.state_machine.change("fall")
+
+    def draw(self):
+        pass
